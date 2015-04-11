b25ca94bc41e562d7eed0673d9ae0684820b32d5
Derek Wisong <derek@wisong.net>
2015-04-11 11:27:03 -0400

    added aquaman.py and logging.ini. added timer infrastructure to power.py

---
+++ aquaman.py
@@ -0,0 +1,26 @@
+import power
+import datetime
+import logging
+
+
+
+def done(when, device, powerstate):
+    log = logging.getLogger(__name__)
+    log.info("Power to device '%s' has been set to %s" % (device, powerstate))
+
+if __name__ == '__main__':
+    import logging.config
+    logging.config.fileConfig('logging.ini')
+    log = logging.getLogger(__name__)
+    #now = datetime.datetime.utcnow()
+    #then = now + datetime.timedelta(seconds=2)
+    #power.schedule_off(1, then, callback=done)
+
+    on = [datetime.time(hour=12, minute=0)]
+    off = [datetime.time(hour=15, minute=0)]
+    s = power.PowerScheduler(1,on, off)
+
+    log.debug("Current event is %s" % str(s.current()))
+    log.debug("Next event is %s" % str(s.next()))
+
+    s.start()
