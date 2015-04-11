bf10df1b62b4336f1be456fa1228625b6fe64837
Derek Wisong <derek@wisong.net>
2015-04-11 15:40:49 -0400

    convert scheduled times to UTC

--- aquaman.py
+++ aquaman.py
@@ -1,7 +1,7 @@
 import power
 import datetime
 import logging
-
+import pytz
 
 
 def done(when, device, powerstate):
@@ -18,5 +18,14 @@ if __name__ == '__main__':
 
     on = [datetime.time(hour=12, minute=0)]
     off = [datetime.time(hour=15, minute=0)]
+
+    # localize the schedule and then convert on and off times to UTC
+    local = pytz.timezone ("America/New_York")
+    on = [local.localize(datetime.datetime.combine(datetime.datetime.now().date(),x)) for x in on]
+    on = [x.astimezone(pytz.utc).time() for x in on]
+
+    off = [local.localize(datetime.datetime.combine(datetime.datetime.now().date(),x)) for x in off]
+    off = [x.astimezone(pytz.utc).time() for x in off]
+    
     s = power.PowerScheduler(1,on, off)
     s.start()
